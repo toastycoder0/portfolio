bd279e4efdae00cec0d4c5936a283341d9b46d77
toastycoder0 <re-jos2002@hotmail.com>
2024-06-02 15:14:02 -0500

    feat: added tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: {}
+    colors: {
+      black: '#121417',
+      white: '#FFFFFF',
+      gray: '#9EABB8',
+      'dark-gray': '#3D4754'
+    },
+    maxWidth: {
+      container: '1000px'
+    },
+    fontFamily: {
+      manrope: 'Manrope, sans-serif'
+    }
   },
   plugins: []
 }

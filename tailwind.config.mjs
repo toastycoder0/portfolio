c436713cd8aadc68eb2b8ffd2993e91e913d09da
toastycoder0 <re-jos2002@hotmail.com>
2024-06-02 20:49:39 -0500

    feat: added light blue color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,8 @@ export default {
       white: '#FFFFFF',
       gray: '#9EABB8',
       blue: '#4493f8',
-      'dark-gray': '#3D4754'
+      'dark-gray': '#3D4754',
+      'light-blue': '#7FB3FB'
     },
     maxWidth: {
       container: '950px'

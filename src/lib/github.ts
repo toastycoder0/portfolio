77796e6810d462a3bb726689a99073e887e8c425
toastycoder0 <silentworker36743@joseluria.com>
2024-06-14 12:54:59 -0500

    feat: added get user data function

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -4,12 +4,34 @@ const octokit = new Octokit({
   auth: import.meta.env.TOKEN_GITHUB_API
 })
 
-export const getRepositories = async () => {
-  return await octokit.request('GET /user/repos', {
-    visibility: 'public',
-    affiliation: 'owner',
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
+export const getUserData = async () => {
+  return await octokit.request('GET /user').then(({ data }) => {
+    const { name, bio: description, avatar_url: image } = data
+    return { name: `${name}`, description: `${description}`, image }
   })
+}
+
+export const getRepositories = async () => {
+  return await octokit
+    .request('GET /user/repos', {
+      visibility: 'public',
+      affiliation: 'owner',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      },
+      per_page: 4,
+      page: 1
+    })
+    .then(({ data }) => {
+      return data.map(({ name, html_url: url, description, topics }) => ({
+        name,
+        url,
+        description,
+        topics
+      }))
+    })
+}
+
+export const getPinedRepos = async () => {
+  return await octokit.request('GET /user/')
 }
